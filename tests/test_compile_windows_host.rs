a92398258a207633a77bf165be26e78dfdcb0b03
alambov-md <alex.lambov.md@gmail.com>
2024-03-31 11:11:42 +0300

    [LEP-9] Fix Windows compilation (#5)
    * [LEP-9] Fix Windows compilation

--- tests/test_compile_windows_host.rs
+++ tests/test_compile_windows_host.rs
@@ -6,23 +6,23 @@ use helpers::{test_artifact_build, test_compile};
 #[cfg(target_arch = "x86_64")]
 #[test]
 fn test_compile_x86_64() {
-    test_compile("x86_64-pc-windows-msvs")
+    test_compile("x86_64-pc-windows-msvc")
 }
 
 #[cfg(target_arch = "aarch64")]
 #[test]
 fn test_compile_aarch64() {
-    test_compile("aarch64-pc-windows-msvs")
+    test_compile("aarch64-pc-windows-msvc")
 }
 
 #[cfg(target_arch = "x86_64")]
 #[test]
 fn test_build_and_bindgen_x86_64() {
-    test_artifact_build("x86_64-pc-windows-msvs")
+    test_artifact_build("x86_64-pc-windows-msvc")
 }
 
 #[cfg(target_arch = "aarch64")]
 #[test]
 fn test_build_and_bindgen_x86_64() {
-    test_artifact_build("aarch64-pc-windows-msvs")
+    test_artifact_build("aarch64-pc-windows-msvc")
 }
